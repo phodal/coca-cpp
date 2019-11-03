eb6a78f72e6c8f928c88c47ff0a0355cb413efdf
Phodal Huang <h@phodal.com>
2019-11-03 20:49:40 +0800

    tests: add cpp examples

---
+++ examples/cpp-basic/function-call/function-call.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+void swap(int,int);
+
+int main()
+{
+    int a = 3, b = 4;
+    cout << "a = " << a << ", b = "
+    << b << endl;
+    swap(a,b);
+    cout << "a = " << a << ", b = "
+    << b << endl;
+    return 0;
+}
+
+void swap(int x, int y)
+{
+     int t = x;
+     x = y;
+     y = t;
+}
